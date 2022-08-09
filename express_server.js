15aeaefd7ba37cbcd6253fd0c81c932d8ad58423
Shane <navysupra@gmail.com>
2022-08-09 05:17:00 +0000

    cleaning out some unused code

--- express_server.js
+++ express_server.js
@@ -65,15 +65,6 @@ app.get('/urls.json', (request, response) => {
 app.get('/hello', (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-// GET - set
-app.get('/set', (request, response) => {
-  const a = 1;
-  response.send(`a = ${a}`);
-});
-// GET - fetch
-app.get('/fetch', (request, response) => {
-  response.send(`a = ${a}`);
-});
 app.get(`*`, (request, response) => {
   response.statusCode = 404;
   response.send('Not good bro, not good.');
@@ -90,6 +81,7 @@ app.post('/urls/:id/delete', (request, response) => {
   delete urlDataBase[request.params.id];
   response.redirect('/urls');
 });
+
 
 /* Execution & Test Data */
 app.listen(PORT, () => {
