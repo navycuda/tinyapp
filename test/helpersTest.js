bb86e6cb248b8f713de6b4463b2cc459908a86d2
Shane <navysupra@gmail.com>
2022-08-11 23:08:48 +0000

    Got the tests to work correctly

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -2,6 +2,8 @@ const { assert } = require('chai');
 const { getUidByEmail } = require('../helpers.js');
 const User = require('../User');
 
+const userDatabase = {};
+
 const createUser = (id, username, email, password) => {
   const user = new User(userDatabase, username, email, password);
   user.uid = id;
@@ -9,16 +11,25 @@ const createUser = (id, username, email, password) => {
 };
 
 const uA = createUser('aaa', 'a', 'a@a.com', 'abc');
+userDatabase[uA.uid] = uA;
 const uB = createUser('bbb', 'b', 'b@a.com', 'bcd');
+userDatabase[uB.uid] = uB;
 const uC = createUser('ccc', 'c', 'c@a.com', 'cde');
+userDatabase[uC.uid] = uC;
 const uD = createUser('ddd', 'd', 'd@a.com', 'def');
+userDatabase[uD.uid] = uD;
 
-const userDatabase = { uA, uB, uC, uD };
+// console.log(`userDatabase after initialization\n`, userDatabase);
 
 describe('getUidByEmail', function() {
-  it('should return a user with valid email', function() {
-    const user = getUidByEmail('a@a.com', userDatabase);
-    const expectedUserID = "aaa";
-    assert.strictEqual(user === expectedUserID);
+  it('should return a uid with valid email', () => {
+    const uid = getUidByEmail('a@a.com', userDatabase);
+    const correctUid = "aaa";
+    assert.strictEqual(uid, correctUid);
+  });
+  it('should return null with invalid email', () => {
+    const uid = getUidByEmail('a@n.com', userDatabase);
+    const correctUid = null;
+    assert.strictEqual(uid, correctUid);
   });
 });
